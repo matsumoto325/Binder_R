fcd419e5c4d163198f57ae22ee1a44c3c02a1c14
Jaehyun Song <tintstyle@gmail.com>
2023-03-27 13:40:24 +0900

    Update setup_cran.R

--- setup_cran.R
+++ setup_cran.R
@@ -2,6 +2,4 @@ install.packages("daggity",  dependencies = TRUE, repos = "https://cran.ism.ac.j
 install.packages("ggdag",    dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
 install.packages("rdd",      dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
 install.packages("rdrobust", dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
-install.packages("titanic",  dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
-install.packages("ghibli",   dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
 install.packages("gsynth",   dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
