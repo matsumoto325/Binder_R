3982340667eefe077ad5c1020bd09a784ac0d3f3
Jaehyun Song <tintstyle@gmail.com>
2023-09-19 16:34:20 +0900

    Update setup_cran.R

--- setup_cran.R
+++ setup_cran.R
@@ -1,5 +1,4 @@
 install.packages("daggity",  dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
-install.packages("ggdag",    dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
 install.packages("rdd",      dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
 install.packages("rdrobust", dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
 install.packages("gsynth",   dependencies = TRUE, repos = "https://cran.ism.ac.jp/")
