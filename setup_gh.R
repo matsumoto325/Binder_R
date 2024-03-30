d3721259938b9c6b804ac82aba4874b8d86aa7a1
Jaehyun Song <tintstyle@gmail.com>
2024-03-30 14:30:47 +0900

    Update setup_gh.R

--- setup_gh.R
+++ setup_gh.R
@@ -1,5 +1,5 @@
 remotes::install_github("uribo/jpndistrict")
 remotes::install_github("ropensci/rnaturalearthhires")
-remotes::install_github('Gedevan-Aleksizde/rmdja@development', upgrade = "never")
+#remotes::install_github('Gedevan-Aleksizde/rmdja@development', upgrade = "never")
 
 pacman::p_up()
